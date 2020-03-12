ea0b7ad4b1c80ae95e6864bfef3fc0c4338c51e1
bbbaiqian <58846881+bbbaiqian@users.noreply.github.com>
2020-03-12 14:50:52 +0100

    Update train.py

--- train.py
+++ train.py
@@ -45,7 +45,7 @@ class Trainer:
         for i, (x_array, t_array) in enumerate(self.train_iter):
             device = torch.device("cuda" if cuda.is_available() else "cpu")
             x = x_array.to(device)
-            t = t_array.to(device)
+            t = t_array.to(device, dtype=torch.int64)
             self.optimizer.zero_grad()
             y = self.model(x)
             y = torch.tensor(y, dtype=torch.int64, device=device)
