cc3caa4a9123bacdcf53933c226ee462aded7f73
Rickard Karlsson <35599490+RickardKarl@users.noreply.github.com>
2020-03-12 10:55:00 +0100

    Change import

--- models/convnet.py
+++ models/convnet.py
@@ -4,7 +4,7 @@ import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from convbnrelu import ConvBNReLU
+from models.convbnrelu import ConvBNReLU
 
 
 class ConvNet(nn.Module):
