04841709aca9844035cb6b6a8e47942cbc3161e3
bbbaiqian <58846881+bbbaiqian@users.noreply.github.com>
2020-03-12 14:42:15 +0100

    Update convnet.py

--- models/convnet.py
+++ models/convnet.py
@@ -42,8 +42,7 @@ class ConvNet(nn.Module):
         h = self.conv34(h, self.train)
         h = F.max_pool2d(h, 2)
 
-        print(h.size)
-        h = h.view(-1, h.size(0))
+        h = h.view(h.size(0), -1)
 
         h = F.dropout(F.relu(self.fc4(h)), training=self.train)
         h = F.dropout(F.relu(self.fc5(h)), training=self.train)
