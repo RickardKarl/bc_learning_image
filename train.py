873ac2071fb80aaaed5aded0d4cd8917630bbfd2
bbbaiqian <58846881+bbbaiqian@users.noreply.github.com>
2020-03-13 09:33:15 +0100

    Update train.py

--- train.py
+++ train.py
@@ -48,7 +48,7 @@ class Trainer:
             t = t_array.to(device, dtype=torch.int64)
 
             if self.opt.BC:
-                y = self.model(x)
+                y = F.log_softmax(self.model(x))
                 y = y.to(torch.float32)
                 t = t.to(torch.float32)
                 loss = utils.kl_divergence(y, t)
