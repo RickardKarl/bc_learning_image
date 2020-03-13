4c6d8b3da8dc860cf1b301b1eeb39ce76856bd4f
bbbaiqian <58846881+bbbaiqian@users.noreply.github.com>
2020-03-13 08:27:15 +0100

    Update train.py

--- train.py
+++ train.py
@@ -48,9 +48,8 @@ class Trainer:
             t = t_array.to(device, dtype=torch.int64)
             self.optimizer.zero_grad()
             # TODO: find out softmax_cross_entropy in PyTorch
-            y = F.softmax(self.model(x), dim=1)
+            y = F.log_softmax(self.model(x), dim=1)
             if self.opt.BC:
-                y = torch.tensor(y, dtype=torch.int64)
                 loss = utils.kl_divergence(y, t)
                 acc = accuracy(y, np.argmax(t, axis=1))[0]
             else:
