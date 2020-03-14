c8ff68829e08afd38dbe086b5cd886e8ad55b022
bbbaiqian <58846881+bbbaiqian@users.noreply.github.com>
2020-03-14 11:04:32 +0100

    Update convnet.py

--- models/convnet.py
+++ models/convnet.py
@@ -42,7 +42,8 @@ class ConvNet(nn.Module):
         h = self.conv34(h)
         h = F.max_pool2d(h, 2)
 
-        # h = h.view(h.size(0), -1)
+        # TODO: figure out why would size mismatch
+        h = h.view(h.size(0), -1)
 
         h = F.dropout(F.relu(self.fc4(h)), training=self.train)
         h = F.dropout(F.relu(self.fc5(h)), training=self.train)
