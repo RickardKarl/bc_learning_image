e088e8001d1b77b8b9e9e3c54f45707269cb021b
Rickard Karlsson <35599490+RickardKarl@users.noreply.github.com>
2020-03-18 12:37:56 +0100

    Changed kl_divergence back to torch function

--- utils.py
+++ utils.py
@@ -46,13 +46,14 @@ def zero_mean(mean, std):
 
 
 def kl_divergence(y, t):
-    entropy = F.softmax(t, dim=1) * F.log_softmax(t, dim=1)
-    entropy = - entropy.sum()
-    crossEntropy = t * F.log_softmax(y, dim=1)
-    crossEntropy = - crossEntropy.sum()
+
+    #entropy = F.softmax(t, dim=1) * F.log_softmax(t, dim=1)
+    #entropy = - entropy.sum()
+    #crossEntropy = t * F.log_softmax(y, dim=1)
+    #crossEntropy = - crossEntropy.sum()
+    #return torch.div((crossEntropy - entropy), y.shape[0])
     
-    return torch.div((crossEntropy - entropy), y.shape[0])
-    #return F.kl_div(y, t, reduction="batchmean")
+    return F.kl_div(y, t, reduction="batchmean")
 
 
 def to_hms(time):
