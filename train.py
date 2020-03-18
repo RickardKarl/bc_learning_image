656b7eee3f0d2470ae4cb3a72624e16afdddf963
Rickard Karlsson <35599490+RickardKarl@users.noreply.github.com>
2020-03-18 10:14:28 +0100

    changed accuracy, add no_grad() in val

--- train.py
+++ train.py
@@ -4,6 +4,7 @@ import torch
 from torch import cuda
 import torch.nn.functional as F
 import time
+from sklearn.metrics import accuracy_score
 
 import utils
 
@@ -12,9 +13,10 @@ def accuracy(y, t):
     """ Computes the multiclass classification accuracy """
     pred = y.argmax(axis=1).reshape(t.shape)
 
-    count = (pred == t).sum()
-    acc = np.asarray(float(count) / len(t.data))
-
+    #count = (pred == t).sum()
+    #acc = np.asarray(float(count) / len(t.data))
+    acc = accuracy_score(t, pred)
+    
     return acc
 
 
@@ -38,23 +40,24 @@ class Trainer:
         train_acc = 0
         for i, (x_array, t_array) in enumerate(self.train_iter):
             device = torch.device("cuda" if cuda.is_available() else "cpu")
+            self.optimizer.zero_grad()
+
             x = x_array.to(device)
             t = t_array.to(device)
+            y = self.model(x)
             if self.opt.BC:
-                y = self.model(x)
                 t = t.to(device, dtype=torch.float32)
                 y = y.to(device, dtype=torch.float32)
                 loss = utils.kl_divergence(y, t)
                 t_values, t_indices = torch.max(t, dim=1)
                 acc = accuracy(y.data, t_indices)
             else:
-                t = t.to(device, dtype=torch.int64)
-                y = self.model(x)
                 """ F.cross_entropy already combines log_softmax and NLLLoss """
+                t = t.to(device, dtype=torch.int32)
                 loss = F.cross_entropy(y, t)
                 acc = accuracy(y.data, t)
 
-            self.optimizer.zero_grad()
+            
             loss.backward()
             self.optimizer.step()
 
@@ -81,10 +84,14 @@ class Trainer:
         val_acc = 0
         for (x_array, t_array) in self.val_iter:
             device = torch.device("cuda" if cuda.is_available() else "cpu")
-            x = x_array.to(device)
-            t = t_array.to(device, dtype=torch.int64)
-            # TODO: figure out why to use softmax here since it also works fine without softmax
-            y = F.softmax(self.model(x), dim=1)
+
+            # Disable gradient computation during validation
+            with torch.no_grad(): 
+                x = x_array.to(device)
+                t = t_array.to(device, dtype=torch.int64)
+                # TODO: figure out why to use softmax here since it also works fine without softmax
+                y = F.softmax(self.model(x), dim=1)
+
             acc = accuracy(y.data, t)
             val_acc += float(acc.item()) * len(t.data)
 
