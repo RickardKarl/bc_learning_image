a76eb953f81d3f1942bedf6aee798434831351bd
Rickard Karlsson <35599490+RickardKarl@users.noreply.github.com>
2020-04-08 17:24:35 +0200

    add vgg19

--- opts.py
+++ opts.py
@@ -50,7 +50,8 @@ def parse():
         'convnet': {'nEpochs': 250, 'LR': 0.1, 'schedule': [0.4, 0.6, 0.8], 'warmup': 0, 'batchSize': 128}
     }
     default_settings['caltech101'] = {
-        'convnet': {'nEpochs': 50, 'LR': 0.1, 'schedule': [0.4, 0.6, 0.8], 'warmup': 0, 'batchSize': 32}
+        'convnet': {'nEpochs': 50, 'LR': 0.1, 'schedule': [0.4, 0.6, 0.8], 'warmup': 0, 'batchSize': 32},
+        'vgg19': {'nEpochs': 50, 'LR': 0.1, 'schedule': [0.4, 0.6, 0.8], 'warmup': 0, 'batchSize': 32}
     }
 
     for key in ['nEpochs', 'LR', 'schedule', 'warmup', 'batchSize']:
