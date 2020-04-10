054cdfb656ea6de3ce9c0638fc58a09d5628e3f1
Rickard Karlsson <35599490+RickardKarl@users.noreply.github.com>
2020-04-10 16:52:01 +0200

    debug

--- dataset.py
+++ dataset.py
@@ -97,7 +97,7 @@ class ImageDataset(torch.utils.data.Dataset):
             image = self.preprocess(image).astype(np.float32)
             label = np.array(label, dtype=np.int32)
 
-        print(image)
+        print(image, label, np.max(image))
         return image, label
 
 
