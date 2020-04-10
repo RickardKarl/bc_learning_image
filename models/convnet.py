56034d6c1535f2924984811f29011a28bb9c0aed
Erik Handberg <erikhand@kth.se>
2020-04-10 23:20:11 +0200

    Changes print

--- models/convnet.py
+++ models/convnet.py
@@ -25,7 +25,8 @@ class ConvNet(nn.Module):
         self.train = True
 
     def forward(self, x):
-        print(x.dim())
+
+        print(type(x))
         h = self.conv11(x)
         h = self.conv12(h)
         h = F.max_pool2d(h, 2)
