1bba7c90d79ec181cb3595e58dfcf6dbc88917ff
Rickard Karlsson <35599490+RickardKarl@users.noreply.github.com>
2020-04-10 10:31:36 +0200

    debug

--- process_caltech.py
+++ process_caltech.py
@@ -6,10 +6,9 @@ from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 
-# TODO: Save learning loss
-
 train_size = 0.8
 excluded_labels = ['BACKGROUND_Google'] # there exists Faces_easy which we only use for now
+oversampled_images = ['motorbikes', 'Faces_easy', 'Faces', 'airplanes']
 basewidth = 224
 hsize = 224
 
@@ -31,6 +30,7 @@ if __name__ == "__main__":
             for f in files:
                 flist.append(join(label_path, f))
             label_to_image[l] = flist
+
     # Map label to int
     label_to_int = {}
     index = 0
@@ -46,8 +46,14 @@ if __name__ == "__main__":
 
     # Go through images
     for l, image_paths in label_to_image.items():
-        nbr_training_samples = int(len(image_paths)*train_size)
 
+        if l in oversampled_images:
+            total_training_samples = 300
+        else:
+            total_training_samples = len(image_paths)
+        
+        nbr_training_samples = int(0.8*total_training_samples)
+        
         # Training data
         for i in range(nbr_training_samples):
             p = image_paths[i]
@@ -60,7 +66,7 @@ if __name__ == "__main__":
             labels_train.append(label_to_int.get(l))
 
         # Test data
-        for i in range(nbr_training_samples, len(image_paths)):
+        for i in range(nbr_training_samples, total_training_samples):
             p = image_paths[i]
             # Get image
             image = Image.open(p).convert('RGB') # get RGB image
@@ -70,19 +76,22 @@ if __name__ == "__main__":
             images_test.append(arr)
             labels_test.append(label_to_int.get(l))
 
-    plt.imshow(images_train[1000])
-    print(labels_train[1000])
-    print(label_to_int)
-    plt.show()
-
     # Concatenate list of arrays into single array
     images_train = np.array(images_train).reshape((-1, 3,basewidth, hsize))
     images_test = np.array(images_test).reshape((-1, 3, basewidth, hsize))
 
-    #print("Mean RGB of images:")
-    #print(train_size*np.mean(images_train, axis=(0, 2, 3)) + (1-train_size)*np.mean(images_test, axis=(0, 2, 3)))
-    #print("Std RGB of images:")
-    #print(np.std(images_test, axis=(0, 2, 3)))
+    print(np.unique(labels_test, return_counts=True))
+
+    print("Channel-wise mean RGB of images:")
+    print(train_size*np.mean(images_train, axis=(0, 2, 3)) + (1-train_size)*np.mean(images_test, axis=(0, 2, 3)))
+    print("Channel-wise std RGB of images:")
+    print(np.std(images_test, axis=(0, 2, 3)))
+
+    print("Per-image mean RGB of images:")
+    print(train_size*np.mean(images_train, axis=(0,1,2,3)) + (1-train_size)*np.mean(images_test, axis=(0,1,2,3)))
+    print("Per-image std RGB of images:")
+    print(np.std(images_test, axis=(0,1,2,3)))
+
 
     # Save in dict
     dataset_dict_train = {}
