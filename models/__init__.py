0461eb3d4f876cb5cfb2755ce127d876c8c910bd
Erik Handberg <erikhand@kth.se>
2020-04-13 14:26:02 +0200

    Add notework

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,6 @@
 from models.convnet import ConvNet as convnet0
+from models.convnet1 import ConvNet as convnet1
 from models.convnet2 import ConvNet as convnet2
+from models.convnet3 import ConvNet as convnet3
+from models.convnet4 import ConvNet as convnet4
+from models.convnet5 import ConvNet as convnet5
